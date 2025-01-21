d5bd0fffc4578817424e13e57f445a3055f6cac5
uhufor <haejung83@gmail.com>
2025-01-21 14:11:43 +0900

    add no op dispatcher

--- ui-event-annotation-processor/src/main/java/com/uhufor/udf/dispatcher/GeneratedUiEventDispatcher.kt
+++ ui-event-annotation-processor/src/main/java/com/uhufor/udf/dispatcher/GeneratedUiEventDispatcher.kt
@@ -3,3 +3,9 @@ package com.uhufor.udf.dispatcher
 interface GeneratedUiEventDispatcher {
     fun dispatchEvent(instance: Any, event: Any?)
 }
+
+object NoOpGeneratedUiEventDispatcher : GeneratedUiEventDispatcher {
+    override fun dispatchEvent(instance: Any, event: Any?) {
+        throw NotImplementedError("Replace this by generated dispatcher.")
+    }
+}
