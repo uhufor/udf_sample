5c5295464a1e5732fd6ce25f191adec7eae13980
uhufor <haejung83@gmail.com>
2025-03-20 00:05:11 +0900

    hidden - support a nested fragment case

--- app/src/main/java/com/uhufor/udf/debug/DebugLabelInitializer.kt
+++ app/src/main/java/com/uhufor/udf/debug/DebugLabelInitializer.kt
@@ -8,6 +8,7 @@ import android.graphics.Color
 import android.graphics.ColorFilter
 import android.graphics.Paint
 import android.graphics.PixelFormat
+import android.graphics.Rect
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.text.TextPaint
@@ -38,11 +39,12 @@ private class LabelDrawable(
     context: Context,
     private val label: String,
     private val alignLeft: Boolean,
+    depth: Int,
     textColor: Int,
     backgroundColor: Int,
 ) : Drawable() {
     private val textPaint = TextPaint(Paint.ANTI_ALIAS_FLAG).apply {
-        textSize = context.dpToPx(TEXT_SIZE).toFloat()
+        textSize = context.dpToPx(FONT_SIZE).toFloat()
         color = textColor
     }
 
@@ -50,18 +52,26 @@ private class LabelDrawable(
         color = backgroundColor
     }
 
+    private val borderPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.BLACK
+        alpha = (255 * 0.3f).toInt()
+        strokeWidth = context.dpToPx(1).toFloat()
+    }
+
     private val textWidth: Int =
-        (textPaint.measureText(label) + context.dpToPx(TEXT_HORIZONTAL_MARGIN)).toInt()
-    private val textHeight: Int = context.dpToPx(TEXT_HEIGHT)
-    private val textOffsetX: Float = context.dpToPx(TEXT_HORIZONTAL_MARGIN).toFloat()
-    private val textOffsetY: Float = context.dpToPx(TEXT_SIZE).toFloat()
+        (textPaint.measureText(label) + context.dpToPx(HORIZONTAL_MARGIN)).toInt()
+    private val textHeight: Int = context.dpToPx(LABEL_HEIGHT)
+    private val textOffsetX: Float = context.dpToPx(HORIZONTAL_MARGIN).toFloat()
+    private val textOffsetY: Float = context.dpToPx(FONT_SIZE).toFloat()
+    private val offsetY: Float = context.dpToPx(LABEL_HEIGHT * depth).toFloat()
 
     override fun draw(canvas: Canvas) {
         val left = if (alignLeft) 0f else (bounds.right - textWidth).toFloat()
-        val top = 0f
+        val top = offsetY
         val right = left + textWidth
         val bottom = top + textHeight
         canvas.drawRect(left, top, right, bottom, backgroundPaint)
+        canvas.drawLine(left, bottom.toFloat(), right, bottom.toFloat(), borderPaint)
 
         val textX = if (alignLeft) left + textOffsetX else left
         val textY = top + textOffsetY
@@ -71,11 +81,13 @@ private class LabelDrawable(
     override fun setAlpha(alpha: Int) {
         textPaint.alpha = alpha
         backgroundPaint.alpha = alpha
+        borderPaint.alpha = alpha
     }
 
     override fun setColorFilter(colorFilter: ColorFilter?) {
         textPaint.colorFilter = colorFilter
         backgroundPaint.colorFilter = colorFilter
+        borderPaint.colorFilter = colorFilter
     }
 
     @Deprecated("Deprecated in Java")
@@ -84,9 +96,9 @@ private class LabelDrawable(
     }
 
     companion object {
-        private const val TEXT_HEIGHT = 18
-        private const val TEXT_SIZE = 12
-        private const val TEXT_HORIZONTAL_MARGIN = 16
+        private const val LABEL_HEIGHT = 16
+        private const val FONT_SIZE = 12
+        private const val HORIZONTAL_MARGIN = 16
     }
 }
 
@@ -111,6 +123,7 @@ private class DebugLabelActivityLifecycleCallbacks : Application.ActivityLifecyc
                 context = activity,
                 label = activity.javaClass.simpleName,
                 alignLeft = true,
+                depth = 0,
                 textColor = Color.WHITE,
                 backgroundColor = Color.argb(100, 0, 0, 0),
             )
@@ -134,14 +147,40 @@ private class DebugLabelFragmentLifecycleCallbacks : FragmentManager.FragmentLif
 
     override fun onFragmentResumed(fm: FragmentManager, f: Fragment) {
         val rootView = f.view ?: return
-        if (rootView.foreground == null) {
-            rootView.foreground = LabelDrawable(
-                context = f.requireContext(),
-                label = f.javaClass.simpleName,
-                alignLeft = false,
-                textColor = Color.RED,
-                backgroundColor = Color.argb(100, 180, 180, 255),
-            )
+
+        rootView.post {
+            if (rootView.foreground == null) {
+                rootView.foreground = LabelDrawable(
+                    context = f.requireContext(),
+                    label = f.javaClass.simpleName,
+                    alignLeft = false,
+                    depth = f.computeFragmentDepth(),
+                    textColor = Color.RED,
+                    backgroundColor = Color.argb(100, 180, 180, 255),
+                )
+            }
         }
+    }
+
+    private fun Fragment.computeFragmentDepth(): Int {
+        val childRect = Rect()
+        view?.getGlobalVisibleRect(childRect)
+        if (childRect.isEmpty) return 0
+        val childTop = childRect.top
+
+        var depth = 0
+        var parentFragment = this.parentFragment
+
+        while (parentFragment != null) {
+            val parentRect = Rect()
+            parentFragment.view?.getGlobalVisibleRect(parentRect)
+
+            if (!parentRect.isEmpty && parentRect.top == childTop) {
+                depth++
+            }
+
+            parentFragment = parentFragment.parentFragment
+        }
+        return depth
     }
 }
